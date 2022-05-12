9831e3b259e930f43fea3875d49838484ee58b94
MohamedDiopGit <diopm.contact@gmail.com>
2022-05-12 18:17:16 +0200

    Scroll features

--- js/box_show.js
+++ js/box_show.js
@@ -1,7 +1,21 @@
 const menuToggle = document.querySelector('.toggle')
 const showcase = document.querySelector('.showcase')
+const navigation = document.querySelector('.nav')
+
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active')
     showcase.classList.toggle('active')
 })
+
+window.addEventListener('scroll', () =>{
+    if(window.scrollY > 3){
+        navigation.classList.add('anim-nav');
+        navigation.classList.add('anim-nav');
+    }
+    else {
+        navigation.classList.remove('anim-nav');
+    }
+
+}
+);
