a71af5e1ce0689b60624147c955b1df30492b90a
MohamedDiopGit <diopm.contact@gmail.com>
2022-05-12 18:54:04 +0200

    Reorganizing codes

--- js/box_show.js
+++ js/box_show.js
@@ -1,6 +1,5 @@
 const menuToggle = document.querySelector('.toggle')
 const showcase = document.querySelector('.showcase')
-const navigation = document.querySelector('.nav')
 
 
 menuToggle.addEventListener('click', () => {
@@ -8,14 +7,3 @@ menuToggle.addEventListener('click', () => {
     showcase.classList.toggle('active')
 })
 
-window.addEventListener('scroll', () =>{
-    if(window.scrollY > 3){
-        navigation.classList.add('anim-nav');
-        navigation.classList.add('anim-nav');
-    }
-    else {
-        navigation.classList.remove('anim-nav');
-    }
-
-}
-);
---
+++ js/nav_bar.js
@@ -0,0 +1,14 @@
+const navigation = document.querySelector('.nav')
+
+
+window.addEventListener('scroll', () =>{
+    if(window.scrollY > 3){
+        navigation.classList.add('anim-nav');
+        navigation.classList.add('anim-nav');
+    }
+    else {
+        navigation.classList.remove('anim-nav');
+    }
+
+}
+);
