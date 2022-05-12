614fadfdeaef4b2473ea03f75fc4a4b8f02d9b56
MohamedDiopGit <diopm.contact@gmail.com>
2022-05-12 21:52:35 +0200

    Changes on the welcome string

--- js/typewrite.js
+++ js/typewrite.js
@@ -2,10 +2,13 @@ const txtAnim = document.querySelector('.h2')
 
 console.log(new Typewriter(txtAnim));
 new Typewriter(txtAnim,{
-    deleteSpeed: 20
+    deleteSpeed: 200
 })
 
 .typeString('Welcome to my website')
 .pauseFor(300)
-.typeString('<strong>, I\'m Mohamed Diop</strong>')
+.typeString(', :)')
+.pauseFor(300)
+.deleteChars(4)
+.pauseFor(300)
 .start()
